6f4fcf7c93d73dbd39bfbb9564088bc37ed67c15
evsriharshar <88794339+evsriharshar@users.noreply.github.com>
2021-08-16 11:23:03 +0530

    swagger configuration

---
+++ Learn-main/src/test/java/com/learn/training/SpringbootcrudjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
